5de0d490d1f183db85e4268996a094603d8f72fa
Óscar Sancho <oscar.sancho.gonzalez@gmail.com>
2023-11-29 23:05:29 -0300

    Refactor index.css and main.jsx files

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import { Provider } from 'react-redux'
+import store from './redux/store'
+import { BrowserRouter } from 'react-router-dom'
 
 import '@fontsource/roboto/300.css'
 import '@fontsource/roboto/400.css'
@@ -11,6 +14,10 @@ import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>,
 )
